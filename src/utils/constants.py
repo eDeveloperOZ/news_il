238a1bcc4228e1dc19f56835a24ecc77f3ec82da
ofir ozeri <ofiroz91@gmail.com>
2023-12-01 01:59:39 +0200

    feat:first lunch of beta!!!

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 import os
 
-DEV = True
+DEV = False
 if DEV:
     load_dotenv()
     ACCESS_TOKEN=os.getenv("API_NEWSI_LBOT")
 else:
+    print('prod')
     ACCESS_TOKEN=os.getenv("API_NEWSI_LBOT")
 
 CHANNEL_ID = -1002099984327
